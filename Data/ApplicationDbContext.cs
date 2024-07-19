e0936ed287e3ddebc8bdf2c2b3f80c364e8bb308
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2024-07-19 12:06:50 -0400

    Initial commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using assignment_3.Models;
+
+namespace assignment_3.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,11 @@
+namespace assignment_3.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Image { get; set; }
+    public double Cost { get; set; }
+    public double ShippingCost { get; set; }
+}
